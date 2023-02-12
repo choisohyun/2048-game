b33d11c37ec9bb6c95773eb667af4390606451aa
hoolie-triple <sohyun4283@naver.com>
2023-02-12 15:57:46 +0900

    feat: add local storage util

---
+++ src/utils/storage.ts
@@ -0,0 +1,15 @@
+export const getLocalStorage = () => {
+    const getItem = (keyName: string) => {
+        return window.localStorage.getItem(keyName) || ''
+    }
+
+    const setItem = (keyName: string, value: string) => {
+        window.localStorage.setItem(keyName, value)
+    }
+
+    const removeItem = (keyName: string) => {
+        window.localStorage.removeItem(keyName)
+    }
+
+    return {getItem, setItem, removeItem}
+}
