4a5e1bf9f5835a2a455ee7204cdce811a618a5da
hoolie-triple <sohyun4283@naver.com>
2023-02-12 15:59:16 +0900

    feat: add board context

---
+++ src/contexts/board.tsx
@@ -0,0 +1,32 @@
+
+
+import React, {useContext} from "react";
+
+export const BoardContext = React.createContext({
+    containerWidth: 0,
+    tileCount: 4,
+});
+
+type Props = {
+    containerWidth: number;
+    tileCount: number;
+    children: any;
+};
+
+export const BoardProvider = ({
+                                  children,
+                                  containerWidth = 0,
+                                  tileCount = 4,
+                              }: Props) => {
+    return (
+        <BoardContext.Provider value={{ containerWidth, tileCount }}>
+            {children}
+        </BoardContext.Provider>
+    );
+};
+
+export const useBoard = () => {
+    const { containerWidth, tileCount } = useContext(BoardContext);
+
+    return [containerWidth, tileCount] as [number, number];
+};
