e18c029955e2f6f40f6ef809f85b2965bbfed00f
hoolie-triple <sohyun4283@naver.com>
2023-02-12 15:57:06 +0900

    feat: add grid UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,90 +1,41 @@
-import Image from 'next/image'
+'use client';
+
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import {useEffect, useState} from "react";
+import {getLocalStorage} from "@/src/utils/storage";
+import Block from "@/src/components/block";
 
 const inter = Inter({ subsets: ['latin'] })
 
+const NEW_RECORD = 'new-record'
+
 export default function Home() {
-  return (
+    const [newRecord, setNewRecord] = useState(0);
+    const [blocks, setBlocks] = useState(Array.from(Array(16).keys()));
+
+    useEffect(() => {
+        const {getItem} = getLocalStorage()
+        const initRecord = Number(getItem(NEW_RECORD))
+        initRecord && setNewRecord(initRecord)
+    }, [])
+
+    useEffect(() => {
+        const { setItem } = getLocalStorage()
+        setItem(NEW_RECORD, newRecord.toString())
+    }, [newRecord])
+
+    return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          Let&apos;s play funny 2048 game
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+          <h2 className={inter.className}>Your Best Score is <span className={styles.red}>{newRecord}</span></h2>
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {blocks.map((value) => (<Block key={value} value={value}/>))}
       </div>
     </main>
   )
